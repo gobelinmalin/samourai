29769e3e8a6afab2b47bbb7c651dd339d259b384
jeyofdev <jgregoire.pro@gmail.com>
2020-11-20 12:38:46 +0100

    add perso to Choice component

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const port = process.env.PORT || 3000;
-const routes = require("./routes");
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 3001
+const routes = require('./routes')
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 
-app.use("/api", routes);
+app.use('/api', routes)
 
-app.listen(port, err => {
-  if (err) {
-    throw err;
-  }
+app.listen(port, (err) => {
+    if (err) {
+        throw err
+    }
 
-  console.log("Super ça marche");
-});
+    console.log('Super ça marche')
+})
--- routes/problems.js
+++ routes/problems.js
@@ -1,15 +1,30 @@
-const express = require("express");
-const router = express.Router();
-const connection = require("../config");
+const express = require('express')
+const router = express.Router()
+const connection = require('../config')
 
-router.get("/", (req, res) => {
-  connection.query("SELECT * FROM problem", (err, results) => {
-    if (err) {
-      res.sendStatus(500);
-    } else {
-      res.json(results);
-    }
-  });
-});
+router.get('/', (req, res) => {
+    connection.query('SELECT * FROM problem', (err, results) => {
+        if (err) {
+            res.sendStatus(500)
+        } else {
+            res.json(results)
+        }
+    })
+})
 
-module.exports = router;
+router.get('/:id/resolves', (req, res) => {
+    const id = req.params.id
+    connection.query(
+        'SELECT * FROM resolve WHERE id_problem = ?',
+        [id],
+        (err, results) => {
+            if (err) {
+                res.sendStatus(500)
+            } else {
+                res.json(results)
+            }
+        }
+    )
+})
+
+module.exports = router
